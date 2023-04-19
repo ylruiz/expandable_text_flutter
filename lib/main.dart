d3fb4b94282ff3c9018ae28a7c2090aec85bd673
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2023-04-19 17:44:57 +0200

    feat(improvement): Use slivers instead of  ListView with shrinkWrap set to true

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.light(),
+      theme: ThemeData.light().copyWith(
+        checkboxTheme: const CheckboxThemeData(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(3)),
+          ),
+          splashRadius: 0,
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('ExpandableText'),
@@ -32,14 +40,19 @@ class MyForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (_, index) {
-        return _CheckboxListTileWithExpandableText(
-          text: _checkboxListTileTitles[index],
-        );
-      },
-      itemCount: 4,
-      shrinkWrap: true,
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return _CheckboxListTileWithExpandableText(
+                text: _checkboxListTileTitles[index],
+              );
+            },
+            childCount: _checkboxListTileTitles.length,
+          ),
+        ),
+      ],
     );
   }
 }
@@ -61,72 +74,83 @@ class _CheckboxListTileWithExpandableTextState
 
   @override
   Widget build(BuildContext context) {
-    return CheckboxListTile(
-      controlAffinity: ListTileControlAffinity.leading,
-      title: LayoutBuilder(
-        builder: (_, constraints) {
-          final text = widget.text;
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Checkbox(
+          value: _checkboxValue,
+          onChanged: (_) => setState(() {
+            _checkboxValue = !_checkboxValue;
+          }),
+        ),
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 8, 12, 8),
+            child: LayoutBuilder(
+              builder: (_, constraints) {
+                final text = widget.text;
 
-          TextPainter textPainter = TextPainter(
-            text: TextSpan(text: text),
-            textAlign: TextAlign.start,
-            textDirection: Directionality.of(context),
-            textScaleFactor: MediaQuery.textScaleFactorOf(context),
-          );
-          textPainter.layout(
-              minWidth: constraints.minWidth, maxWidth: constraints.maxWidth);
-          final lineHeight = textPainter.preferredLineHeight;
-          final textHeight = textPainter.size.height;
-          final hasTextMoreThanThreeLines = textHeight >= lineHeight * 4;
+                TextPainter textPainter = TextPainter(
+                  text: TextSpan(text: text),
+                  textAlign: TextAlign.start,
+                  textDirection: Directionality.of(context),
+                  textScaleFactor: MediaQuery.textScaleFactorOf(context),
+                );
+                textPainter.layout(
+                  minWidth: constraints.minWidth,
+                  maxWidth: constraints.maxWidth,
+                );
+                final lineHeight = textPainter.preferredLineHeight;
+                final textHeight = textPainter.size.height;
+                final hasTextMoreThanThreeLines = textHeight >= lineHeight * 4;
 
-          const textStyle = TextStyle(color: Colors.black);
+                const textStyle = TextStyle(color: Colors.black);
 
-          if (!hasTextMoreThanThreeLines) {
-            return Text(
-              text,
-              style: textStyle,
-            );
-          }
+                if (!hasTextMoreThanThreeLines) {
+                  return Text(
+                    text,
+                    style: textStyle,
+                  );
+                }
 
-          return GestureDetector(
-            onTap: () => setState(() {
-              _isExpanded = !_isExpanded;
-            }),
-            child: AnimatedSize(
-              duration: const Duration(milliseconds: 200),
-              curve: Curves.fastLinearToSlowEaseIn,
-              alignment: Alignment.topLeft,
-              child: _isExpanded
-                  ? Text(
-                      text,
-                      style: textStyle,
-                    )
-                  : ShaderMask(
-                      shaderCallback: (Rect bounds) {
-                        return const LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            Colors.black,
-                            Colors.transparent,
-                          ],
-                          tileMode: TileMode.mirror,
-                        ).createShader(bounds);
-                      },
-                      child: Text(
-                        text,
-                        style: textStyle,
-                        maxLines: _isExpanded ? null : 3,
-                      ),
-                    ),
+                return GestureDetector(
+                  onTap: () => setState(() {
+                    _isExpanded = !_isExpanded;
+                  }),
+                  child: AnimatedSize(
+                    duration: const Duration(milliseconds: 200),
+                    curve: Curves.fastLinearToSlowEaseIn,
+                    alignment: Alignment.topLeft,
+                    child: _isExpanded
+                        ? Text(
+                            text,
+                            style: textStyle,
+                          )
+                        : ShaderMask(
+                            shaderCallback: (Rect bounds) {
+                              return const LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  Colors.black,
+                                  Colors.transparent,
+                                ],
+                                tileMode: TileMode.mirror,
+                              ).createShader(bounds);
+                            },
+                            child: Text(
+                              text,
+                              style: textStyle,
+                              maxLines: _isExpanded ? null : 3,
+                            ),
+                          ),
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
-      value: _checkboxValue,
-      onChanged: (_) => setState(() {
-        _checkboxValue = !_checkboxValue;
-      }),
+          ),
+        ),
+      ],
     );
   }
 }
